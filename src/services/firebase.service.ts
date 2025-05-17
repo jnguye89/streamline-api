fc17eb61ce172e09f142dfe7e5f99c2520f085d5
Jackie Nguyen <jnguye89@gmail.com>
2025-05-17 13:39:28 -0700

    put service account into key

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -10,22 +10,14 @@ export class FirebaseService implements OnModuleInit {
   constructor(private readonly config: ConfigService) {}
 
   onModuleInit() {
-    const privateKey = this.config
-      .get<string>('FIREBASE_PRIVATE_KEY')
-      ?.replace(/\\n/g, '\n');
+    const saJson = process.env.FIREBASE_SA_B64;
+    // console.log('json', saJson);
+    admin.initializeApp({
+      credential: admin.credential.cert(JSON.parse(saJson!)),
+      storageBucket: this.config.get<string>('FIREBASE_STORAGE_BUCKET'),
+    });
 
-    if (admin.apps.length === 0) {
-      admin.initializeApp({
-        credential: admin.credential.cert({
-          projectId: this.config.get<string>('FIREBASE_PROJECT_ID'),
-          clientEmail: this.config.get<string>('FIREBASE_CLIENT_EMAIL'),
-          privateKey,
-        }),
-        storageBucket: this.config.get<string>('FIREBASE_STORAGE_BUCKET'),
-      });
-
-      this.bucket = admin.storage().bucket();
-    }
+    this.bucket = admin.storage().bucket();
   }
 
   getStorageBucket(): Bucket {
