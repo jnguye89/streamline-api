fe06d0b8e778d046338efb1a8cedc3af7a4ac2e3
Jackie Nguyen <jnguye89@gmail.com>
2025-05-29 18:42:26 -0700

    changes

--- src/services/stream.gateway.ts
+++ src/services/stream.gateway.ts
@@ -8,7 +8,7 @@ import {
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
-import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
+import { spawn, ChildProcess } from 'child_process';
 
 @WebSocketGateway({
   cors: {
@@ -22,7 +22,7 @@ export class StreamGateway
   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
   private readonly logger = new Logger(StreamGateway.name);
-  private ffmpegMap = new Map<string, ChildProcessWithoutNullStreams>();
+  private ffmpegMap = new Map<string, ChildProcess>();
 
   afterInit(server: Server) {
     server.on('connection', (socket) => {
@@ -38,44 +38,40 @@ export class StreamGateway
   handleConnection(client: Socket) {
     this.logger.log(`Client connected ${client.id}`);
 
-    const ffmpeg = spawn(
-      'ffmpeg',
-      [
-        /* INPUT ---------------------------------------------------- */
-        '-loglevel',
-        'error', // quieter log
-        '-f',
-        'webm', // we know the container type now
-        '-i',
-        'pipe:0', // read from stdin
+    const ffmpeg = spawn('ffmpeg', [
+      /* INPUT ---------------------------------------------------- */
+      '-loglevel',
+      'error', // quieter log
+      '-f',
+      'webm', // we know the container type now
+      '-i',
+      'pipe:0', // read from stdin
 
-        /* TRANSCODE ------------------------------------------------ */
-        // Video: re-encode to H.264 baseline, tune for realtime
-        '-c:v',
-        'libx264',
-        '-profile:v',
-        'baseline',
-        '-preset',
-        'veryfast',
-        '-tune',
-        'zerolatency',
-        // Audio: re-encode Opus → AAC
-        '-c:a',
-        'aac',
-        '-ar',
-        '48000',
-        '-b:a',
-        '128k',
+      /* TRANSCODE ------------------------------------------------ */
+      // Video: re-encode to H.264 baseline, tune for realtime
+      '-c:v',
+      'libx264',
+      '-profile:v',
+      'baseline',
+      '-preset',
+      'veryfast',
+      '-tune',
+      'zerolatency',
+      // Audio: re-encode Opus → AAC
+      '-c:a',
+      'aac',
+      '-ar',
+      '48000',
+      '-b:a',
+      '128k',
 
-        /* OUTPUT --------------------------------------------------- */
-        '-pix_fmt',
-        'yuv420p',
-        '-f',
-        'flv',
-        'rtmps://3b7d4b5b28f6.global-contribute.live-video.net:443/app/sk_us-west-2_o7Qj4w0Hymqs_v7RiHpdGHgb54zph1fjZAd0aDzRICb', // rtmps://…/app/<stream-key>
-      ],
-      { stdio: ['pipe', 'inherit', 'inherit'] },
-    );
+      /* OUTPUT --------------------------------------------------- */
+      '-pix_fmt',
+      'yuv420p',
+      '-f',
+      'flv',
+      'rtmps://3b7d4b5b28f6.global-contribute.live-video.net:443/app/sk_us-west-2_o7Qj4w0Hymqs_v7RiHpdGHgb54zph1fjZAd0aDzRICb', // rtmps://…/app/<stream-key>
+    ]);
 
     /* ---- GUARD RAILS ----------------------- */
     ffmpeg.on('exit', (code, sig) => {
@@ -93,18 +89,18 @@ export class StreamGateway
   @SubscribeMessage('stream-data')
   handleStream(client: Socket, payload: Buffer) {
     const ff = this.ffmpegMap.get(client.id);
-    if (!ff || !ff.stdin.writable) return;
+    if (!ff || !ff.stdin!.writable) return;
 
     /* back-pressure: if write() returns false pause the socket */
-    if (!ff.stdin.write(payload)) {
-      client.conn.pause();
-      ff.stdin.once('drain', () => client.conn.resume());
+    if (!ff.stdin!.write(payload)) {
+      // client.conn.pause();
+      // ff.stdin.once('drain', () => client.conn.resume());
     }
   }
 
   handleDisconnect(client: Socket) {
     const ff = this.ffmpegMap.get(client.id);
-    ff?.stdin.end();
+    ff?.stdin!.end();
     ff?.kill('SIGINT');
     this.ffmpegMap.delete(client.id);
   }
