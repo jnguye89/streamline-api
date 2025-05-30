c5933f755961afa97632fd2b632072dab2785218
Jackie Nguyen <jnguye89@gmail.com>
2025-05-29 17:56:14 -0700

    Make endpoint public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './auth/public.decorator';
+
 @Controller()
 export class AppController {
   @Get()
+  @Public()
   getHello(): string {
     return 'Hello World!';
   }
